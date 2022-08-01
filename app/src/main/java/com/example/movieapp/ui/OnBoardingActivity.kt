e383306d467a8c3a85aeaf3536721488ac75ced4
roxana <roxi_nicoleta2104@yahoo.com>
2022-08-01 16:13:31 +0300

    fixed onBoarding Activity

--- app/src/main/java/com/example/movieapp/ui/OnBoardingActivity.kt
+++ app/src/main/java/com/example/movieapp/ui/OnBoardingActivity.kt
@@ -6,11 +6,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.movieapp.R
+import com.example.movieapp.SearchActivity
+import com.example.movieapp.ui.actors.ActorRepository
 import com.example.movieapp.ui.actors.ActorsActivity
+import com.example.movieapp.ui.genres.GenreRepository
 import com.example.movieapp.ui.genres.GenresActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class OnBoardingActivity : AppCompatActivity() {
+
+    private val genreRepository = GenreRepository.instance
+    private val actorRepository = ActorRepository.instance
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
@@ -33,5 +45,23 @@ class OnBoardingActivity : AppCompatActivity() {
         fun open(context: Context) {
             context.startActivity(Intent(context, OnBoardingActivity::class.java))
         }
+    }
+
+    private fun verifyIfFilterIsSelected() {
+        GlobalScope.launch(Dispatchers.IO) {
+            val genreCount = genreRepository.getCount()
+            val actorCount = actorRepository.getCount()
+            withContext(Dispatchers.Main) {
+                // operatiuni
+                if (genreCount > 0 && actorCount > 0){
+                    SearchActivity.open(this@OnBoardingActivity)
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        verifyIfFilterIsSelected()
     }
 }
